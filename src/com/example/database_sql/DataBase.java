6d4270bf69b3c22b9ef967133ecb5cede8b4528c
ITLAB <ITLAB@IT>
2017-02-21 10:03:13 +0530

    Fifth

--- src/com/example/database_sql/DataBase.java
+++ src/com/example/database_sql/DataBase.java
@@ -3,6 +3,7 @@ package com.example.database_sql;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBase extends SQLiteOpenHelper{
--- src/com/example/database_sql/MainActivity.java
+++ src/com/example/database_sql/MainActivity.java
@@ -4,24 +4,42 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
-
+	EditText name;
+	 EditText num;
+    Button save,view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText name;
-		final EditText num;
-        Button save,view;
+        
         
         name=(EditText)findViewById(R.id.name);
         num=(EditText)findViewById(R.id.num);
         save=(Button)findViewById(R.id.sbt);
         view=(Button)findViewById(R.id.vbt);
+        final DataBase d=new DataBase(this);
+        save.setOnClickListener(new OnClickListener()
+        {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+		       
+		        String cname=name.getText().toString();
+				String cnum=num.getText().toString();
+		        d.addContact(new Contact(cname,cnum));
+				
+			}
+        	
+        });
+        
         
     }
 
